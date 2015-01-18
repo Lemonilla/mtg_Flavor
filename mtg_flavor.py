d29f8f718d727b9ef393a311ec3d27d36b91a5ac
Lemonilla <ntroscinski@gmail.com>
2015-01-18 12:15:23 -0500

    Add comments

--- mtg_flavor.py
+++ mtg_flavor.py
@@ -17,27 +17,32 @@ consumer_secret=sys.argv[2]
 access_token_key=sys.argv[3]
 access_token_secret=sys.argv[4]
 
-
-
+# Loop forever
 while True:
+        # Catch connection errors
         try:
+                # Define defaults
                 message="........................................................................................................................................................................................................................................................................."
                 card_name=None
                 flavor_text=None
 
+                # loop until a match is found
                 while len(message) > 140 or message == None:
                         while flavor_text == None or card_name == None:
 
+                                # Generate random number
                                 ran = random.randrange(1,60000)
                                 if debug == True:
                                         print ran
 
+                                # Check random number for flavor text
                                 req = requests.get("http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=%d" % ran)
                                 for l in flavor.findall(req.text):
                                     l = l.replace("<div class=\"cardtextbox\" style=\"padding-left:10px;\"><i>","").replace("<div class=\"cardtextbox\" style=\"padding-left:10px;\"><i>","").replace("</i></div><div class='cardtextbox'></i>","\n").replace("</i></div></div>","")
                                     if l <> None:
                                         flavor_text = l.encode('utf8')
 
+                                # Check random number for card name
                                 for n in name.findall(req.text):
                                         if n <> None:
                                                 card_name = n.replace("<span id=\"ctl00_ctl00_ctl00_MainContent_SubContent_SubContentHeader_subtitleDisplay\">","").replace("</span>","").replace("  ","").replace("\n","")
@@ -45,28 +50,38 @@ while True:
                                                 card_name = "["+str(card_name[:-1])+"]"
                                                 card_name = card_name + "\n"
 
+                                # Print debug information
                                 if debug == True:
                                         print card_name
 
+                        # Combine into single string
                         message = card_name + flavor_text
 
+                        # Add hashtage if possable
                         if len(message)+13 < 140:
                                 message = message + hashtag
+
+                        # Check message length and restart if too long
                         if len(message) > 140 or message == None:
                                 message="........................................................................................................................................................................................................................................................................."
                                 card_name=None
                                 flavor_text=None
 
+                # Print debug information
                 if debug == True:
                         print message
 
+                # Post status
                 status = twitter.Api(consumer_key=consumer_key,
                                         consumer_secret=consumer_secret,
                                         access_token_key=access_token_key,
                                         access_token_secret=access_token_secret,
                                         input_encoding='utf-8').PostUpdate(message)
+
+                # Wait for random time between 1 min and 1 hr
                 time.sleep(random.randrange(1*60,1*60*60))
 
+        # Catch connection errors
         except:
-                print "Error, Retrying in 30 Seconds. . ."
+                print "Error, retrying in 30 seconds. . ."
                 time.sleep(30)
