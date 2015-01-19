6d7b6b6f91fa07a85c57a891f3a7873399c7b49b
Lemonilla <ntroscinski@gmail.com>
2015-01-19 16:05:20 -0500

    Edited linux launcher

--- linuxLauncher.sh
+++ linuxLauncher.sh
@@ -5,4 +5,4 @@ consumer_secret=$(head -n 1 ConsumerSecret.txt)
 access_token_key=$(head -n 1 AccessToken.txt)
 access_token_secret=$(head -n 1 AccessTokenSecret.txt)
 
-Python mtg_flavor.py consumer_key consumer_secret access_token_key access_token_secret
+Python mtg_flavor.py $consumer_key $consumer_secret $access_token_key $access_token_secret
